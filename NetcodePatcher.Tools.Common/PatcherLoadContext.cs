8e9596475e5a6d9aafdae1c49a922cef0e30644f
r007 <r007+github@r007.de>
2024-06-24 19:16:53 +0200

    PatcherLoadContxt for net472

--- NetcodePatcher.Tools.Common/PatcherLoadContext.cs
+++ NetcodePatcher.Tools.Common/PatcherLoadContext.cs
@@ -1,10 +1,45 @@
-using System.Linq;
+using System;
 using System.Reflection;
+#if !(NETFRAMEWORK || NETSTANDARD)
 using System.Runtime.Loader;
+using System.Linq;
+#endif
 using Serilog;
 
 namespace NetcodePatcher.Tools.Common;
 
+#if NETFRAMEWORK || NETSTANDARD
+class PatcherLoadContext
+{
+    private AssemblyResolver Resolver;
+    private AppDomain Domain;
+
+    public PatcherLoadContext(string name, AssemblyResolver resolver)
+    {
+        Resolver = resolver;
+        Domain = AppDomain.CurrentDomain;
+        Domain.AssemblyResolve += ResolveHandler;
+    }
+
+    private Assembly? ResolveHandler(object source, ResolveEventArgs args)
+    {
+        Log.Debug("Trying to resolve {name}", args.Name);
+        var assemblyName = new AssemblyName(args.Name);
+        var path = Resolver.ResolveAssemblyToPath(assemblyName);
+        if (path is not null)
+        {
+            Log.Debug("Resolved {name} as {path}", assemblyName.Name, path);
+            return Assembly.LoadFrom(path);
+        }
+        return null;
+    }
+
+    public Assembly LoadFromAssemblyName(AssemblyName name)
+    {
+        return Domain.Load(name);
+    }
+}
+#else
 class PatcherLoadContext : AssemblyLoadContext
 {
     private AssemblyResolver Resolver;
@@ -38,3 +73,4 @@ class PatcherLoadContext : AssemblyLoadContext
         return LoadFromAssemblyPath(assemblyPath);
     }
 }
+#endif
